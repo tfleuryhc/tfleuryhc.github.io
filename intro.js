bd3ff6afe93790d1135c99e790b282cacc7a6828
Tristan Fleury <35713900+tfleuryhc@users.noreply.github.com>
2023-12-06 16:10:55 -0500

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,8 @@
+function revealMessage() {
+ document.getElementById("hiddenMessage").style.display = 'block';
+}
+function terrifyingMessage() {
+ alert("Look Behind You!");
+ document.getElementById("fakeIntro").style.display = 'none';
+ document.getElementById("realIntro").style.display = 'block';
+}
